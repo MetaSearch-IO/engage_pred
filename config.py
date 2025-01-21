b7871d797ee48f46384750200aa7444c6758d703
Liu Boya <liuboya@Lius-Laptop.lan>
2025-01-21 14:52:14 +0800

    fix

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # dataset construction
 num_train_day = 7
 num_val_day = 1
-max_test_size = 50000
+max_test_size = 10000
 
 # model training
 num_epochs = 1
 learning_rate = 5e-6
-batch_size = 512
+batch_size = 256
 log_interval = 1
